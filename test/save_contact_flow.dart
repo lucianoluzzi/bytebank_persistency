3b5e3a0730a388d3973640dac00786386fc9f8d1
Luciano Luzzi <luciano@woovapp.com>
2020-10-21 23:23:07 +0200

    Implementing the save contact test flow.

--- test/save_contact_flow.dart
+++ test/save_contact_flow.dart
@@ -1,11 +1,13 @@
 import 'dart:math';
 
 import 'package:bytebank_persistency/main.dart';
+import 'package:bytebank_persistency/models/contact.dart';
 import 'package:bytebank_persistency/screens/contact_form.dart';
 import 'package:bytebank_persistency/screens/contacts_list.dart';
 import 'package:bytebank_persistency/screens/dashboard.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
 
 import 'dashboard_widget_test.dart';
 import 'mocks.dart';
@@ -28,6 +30,8 @@ void main() {
     final contactsList = find.byType(ContactsList);
     expect(contactsList, findsOneWidget);
 
+    verify(mockContactDAO.findAll());
+
     final fabNewContact = find.widgetWithIcon(FloatingActionButton, Icons.add);
     expect(fabNewContact, findsOneWidget);
     await tester.tap(fabNewContact);
@@ -36,21 +40,11 @@ void main() {
     final contactForm = find.byType(ContactForm);
     expect(contactForm, findsOneWidget);
 
-    final nameTextField = find.byWidgetPredicate((widget) {
-      if (widget is TextField) {
-        return widget.decoration.labelText == 'Full name';
-      }
-      return false;
-    });
+    final nameTextField = find.byWidgetPredicate((widget) => _contactFormLabelMatcher(widget, 'Full name'));
     expect(nameTextField, findsOneWidget);
     await tester.enterText(nameTextField, 'Luzzi');
 
-    final accountNumberTextField = find.byWidgetPredicate((widget) {
-      if (widget is TextField) {
-        return widget.decoration.labelText == 'Account number';
-      }
-      return false;
-    });
+    final accountNumberTextField = find.byWidgetPredicate((widget) => _contactFormLabelMatcher(widget, 'Account number'));
     expect(accountNumberTextField, findsOneWidget);
     await tester.enterText(accountNumberTextField, '12345');
 
@@ -59,7 +53,19 @@ void main() {
     await tester.tap(createButton);
     await tester.pumpAndSettle();
 
+    verify(mockContactDAO.save(Contact(0, 'Luzzi', 12345)));
+
     final contactsListBack = find.byType(ContactsList);
     expect(contactsListBack, findsOneWidget);
+
+    verify(mockContactDAO.findAll());
+    
   });
+}
+
+bool _contactFormLabelMatcher(widget, String labelValue) {
+  if (widget is TextField) {
+    return widget.decoration.labelText == labelValue;
+  }
+  return false;
 }
